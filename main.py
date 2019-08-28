17ce862e973753db90f17c3bc9e3c6ce54681559
Soulzippey <wyattbw@gmail.com>
2019-08-28 18:38:32 -0400

    First Draft

--- main.py
+++ main.py
@@ -5,4 +5,9 @@ import utils
 utils.check_version((3,7))
 utils.clear()
 
-print('Hello, Class!')
+print('My name is Wyatt Wilcox')
+print('My Favorite Game is probably TF2')
+print('I dont really have concerns')
+print('Im excited about learning how game engines work and how to program in the game world a bit better')
+print('My Stackoverflow usernumber 11980890')
+print('My github profile URl is https://github.com/Soulzippey')
